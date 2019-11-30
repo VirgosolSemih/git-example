6e26437659e11ee9acc59307145d1aeab9cfe401
VirgosolSemih <semih.arslan@virgosol.com>
2019-11-30 10:41:46 +0300

    first commit

---
+++ src/main/java/com/virgosol/example/App.java
@@ -0,0 +1,13 @@
+package com.virgosol.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
